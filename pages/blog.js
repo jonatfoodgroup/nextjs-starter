13ba0b77b27fe29f143802dd914d67ad45ddbee9
Jon Senterfitt <jon.senterfitt@thefoodgroup.com>
2023-03-07 03:49:06 -0600

    Added default exports

---
+++ pages/blog.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import config from '@/config'
+import Sidebar from '@/components/Sidebar'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>{config.project_title}</title>
+        <meta name="description" content={config.project_description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <Header />
+        <Sidebar />
+        <Footer />
+      </main>
+    </>
+  )
+}
